d8156fc11a6b9087cdcb628e39ac7ab2ef701056
Ingo Ruhnke <grumbel@gmail.com>
2024-05-26 17:55:25 +0200

    Update to nixos-24.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     openocd_src.url = "git+https://github.com/openocd-org/openocd.git?submodules=1";
@@ -17,6 +17,9 @@
 
           openocd = pkgs.openocd.overrideAttrs (oldAttrs: {
             src = openocd_src;
+
+            enableParallelBuilding = true;
+
             patches = [];
 
             patchPhase = ''
